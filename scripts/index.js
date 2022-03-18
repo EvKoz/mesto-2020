2aa039b02d5bc8b68f55993bacd15cad687f00b6
Evgeny Kozlov <ev.koz@outlook.com>
2022-03-18 22:47:16 +0300

    3rd review

--- scripts/index.js
+++ scripts/index.js
@@ -1,23 +1,23 @@
-let profileElement = document.querySelector(".profile__info");
-let popupElement = document.querySelector(".pop-up");
-let popupCloseButtonElement = popupElement.querySelector(".pop-up__close-icon");
+let profileElement = document.querySelector('.profile__info');
+let popupElement = document.querySelector('.pop-up');
+let popupCloseButtonElement = popupElement.querySelector('.pop-up__close-icon');
 let popupOpenButtonElement = document.querySelector(
-  ".profile__info-edit-button"
+  '.profile__edit-button'
 );
-let inputNameElement = document.querySelector(".pop-up__input-field_type_name");
-let inputJobElement = document.querySelector(".pop-up__input-field_type_role");
-let nameElement = document.querySelector(".profile__info-name");
-let jobElement = document.querySelector(".profile__info-role");
-let saveButton = document.querySelector(".pop-up__input-save");
+let inputNameElement = document.querySelector('.pop-up__input-field_type_name');
+let inputJobElement = document.querySelector('.pop-up__input-field_type_role');
+let nameElement = document.querySelector('.profile__info-name');
+let jobElement = document.querySelector('.profile__info-role');
+let saveButton = document.querySelector('.pop-up__input-save');
 
-let openPopup = function () {
-  popupElement.classList.add("pop-up_opened");
+function openPopup() {
+  popupElement.classList.add('pop-up_opened');
   inputNameElement.value = nameElement.textContent;
   inputJobElement.value = jobElement.textContent;
 };
 
-let closePopup = function () {
-  popupElement.classList.remove("pop-up_opened");
+function closePopup() {
+  popupElement.classList.remove('pop-up_opened');
 };
 
 function editProfile(evt) {
@@ -28,7 +28,7 @@ function editProfile(evt) {
   closePopup();
 }
 
-profileElement.addEventListener("submit", editProfile);
-saveButton.addEventListener("click", editProfile);
-popupOpenButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
+profileElement.addEventListener('submit', editProfile);
+saveButton.addEventListener('click', editProfile);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
