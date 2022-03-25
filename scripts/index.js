097355bc8cd29fcb3c00c0332293c60c8a7f6fbd
Evgeny Kozlov <ev.koz@outlook.com>
2022-03-25 23:35:19 +0300

    place add form

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,23 @@ let popupCloseButtonElement = popupElement.querySelector('.pop-up__close-icon');
 let inputNameElement = document.querySelector('.pop-up__input-field_type_name');
 let inputJobElement = document.querySelector('.pop-up__input-field_type_role');
 let saveButton = document.querySelector('.pop-up__input-save');
+const popupPlaceElement = document.querySelector('.pop-up__type_place')
+
+
+
+function openPlacePopup() {
+  popupPlaceElement.classList.add('pop-up_type_place_opened');
+  /*inputNameElement.value = nameElement.textContent;
+  inputJobElement.value = jobElement.textContent;*/
+};
+
+
+
+function closePlacePopup() {
+  popupElement.classList.remove('pop-up_type_place_opened');
+};
+
+
 
 function openPopup() {
   popupElement.classList.add('pop-up_opened');
@@ -29,10 +46,15 @@ popupOpenButtonElement.addEventListener('click', openPopup);
 profileElement.addEventListener('submit', editProfile);
 saveButton.addEventListener('click', editProfile);
 popupCloseButtonElement.addEventListener('click', closePopup);
+popupPlaceElement.addEventListener('click', openPlacePopup);
+
 
 /*фичи ПР5 */
 
 const createContent = document.querySelector('.content');
+const addPlaceBtn = document.querySelector('.profile__add-button')
+const placeInput = document.querySelector('.pop-up__input-field_type_place');
+const linkInput = document.querySelector('.pop-up__input-field_type_link');
 
 const initialCards = [
   {
@@ -61,19 +83,34 @@ const initialCards = [
   }
 ];
 
+
 const createList = () => {
 
-const item = initialCards.map(el => {
-  return `<li class="content__element">
-  <button class="content__element-delete-button" type="button"></button>
-  <img alt="Одно из любимых мест" class="content__element-image" src="${el.link}"/>
-  <div class="content__element-symbols-container">
-    <h2 class="content__element-text">${el.name}</h2>
-    <button class="content__element-like-button" type="button"></button>
-  </div>
-</li>`;
-}).join(' ');
+const item = initialCards.map(element => {
+  return createCard(element);
+}).join('');
 createContent.insertAdjacentHTML('afterbegin', item);
+};
+
+const createCard = data => {
+return `<li class="content__element">
+<button class="content__element-delete-button" type="button"></button>
+<img alt="Одно из любимых мест" class="content__element-image" src="${data.link}"/>
+<div class="content__element-symbols-container">
+  <h2 class="content__element-text">${data.name}</h2>
+  <button class="content__element-like-button" type="button"></button>
+</div>
+</li>`
+};
+
+const addPlace = () => {
+  addPlaceBtn.addEventListener('click', () => {
+    const card = createCard({
+      name: placeInput.nodevalue,
+      link: linkInput.nodevalue,
+    });
+    createContent.insertAdjacentHTML('afterbegin', card);
+  });
 };
 
 createList();
